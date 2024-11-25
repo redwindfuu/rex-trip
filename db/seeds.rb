4b9583f17fc41e80e34c96b5d4dfb4f03efaf5ea
Zhen Feng <75919476+redwindfuu@users.noreply.github.com>
2024-11-25 11:41:37 +0700

    chore: add seed to create default driver and customer

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,27 @@ admin = Admin.find_or_create_by!(username: 'admin') do |user|
   user.role= 'super_admin'
   user.save!
 end
+
+# Create a default customer user
+if Customer.count == 0
+  customer = Customer.new
+  customer.phone= '1234567890'
+  customer.email = 'mail@gmail.com'
+  customer.full_name = 'John Doe'
+  customer.username = 'johndoe'
+  customer.password = '12345678'
+  customer.password_confirmation = '12345678'
+  customer.save!
+end
+
+# Create a default driver user
+if Driver.count == 0
+  driver = Driver.new
+  driver.phone= '1234567890'
+  driver.email = 'drivermail@gmail.com'
+  driver.full_name = 'Driver Doe'
+  driver.username = 'driverdoe'
+  driver.password = '12345678'
+  driver.password_confirmation = '12345678'
+  driver.save!
+end
